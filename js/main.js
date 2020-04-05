b20a83e0031d95f45542e70fe69a989606125107
UyenDo <uyendtt@minerva.vn>
2020-04-05 14:59:26 +0700

    done not validate

--- js/main.js
+++ js/main.js
@@ -1,171 +1,75 @@
-'use strict';
-
-// Init all plugin when document is ready
-$(document).on('ready', function () {
-	// 0. Init console to avoid error
-	var method;
-	var noop = function () { };
-	var methods = [
-		'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-		'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-		'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-		'timeStamp', 'trace', 'warn'
-	];
-	var length = methods.length;
-	var console = (window.console = window.console || {});
-	var contextWindow = $(window);
-	var $root = $('html, body');
-	while (length--) {
-		method = methods[length];
-		// Only stub undefined methods.
-		if (!console[method]) {
-			console[method] = noop;
-		}
-	}
+var pageAnchors = [];
+var pageSections = [];
+var pageSectionDivs = $('.fullpage .section');
+var mainPage = $('.fullpage');
 
 
-	// 3. Show/hide menu when icon is clicked
-	var menuItems = $('.all-menu-wrapper .nav-link');
-	var menuIcon = $('.menu-icon, #navMenuIcon');
-	var menuBlock = $('.all-menu-wrapper');
-	var reactToMenu = $ ('.page-main, .navbar-sidebar, .page-cover')
-	var menuLinks = $(".navbar-mainmenu a, .navbar-sidebar a");
-	// Menu icon clicked
-	menuIcon.on('click', function () {
-		menuIcon.toggleClass('menu-visible');
-		menuBlock.toggleClass('menu-visible');
-		menuItems.toggleClass('menu-visible');
-		reactToMenu.toggleClass('menu-visible');
-		return false;
-	});
+if (mainPage.width()) {
+    /*config fullpage.js*/
+    mainPage.fullpage({
+        menu: "#navigation",
+        lockAnchors: !1,
+        navigation: true,
+        navigationPosition: 'right',
+        scrollingSpeed: 500,
+        autoScrolling: !0,
+        fitToSection: !0,
+        fitToSectionDelay: 500,
+        scrollBar: !1,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        css3: true,
+        easing: 'easeInOutCubic',
+        loopBottom: !1,
+        loopTop: !1,
+        animateAnchor: !0,
+        recordHistory: !1,
+        controlArrows: !1,
+        verticalCentered: !1,
+        paddingTop: !1,
+        paddingBottom: !1,
+        sectionSelector: ".section",
+        normalScrollElements: '.section-scroll',
+        responsiveWidth: 768,
+        afterResponsive: function(isResponsive){
 
+        },
+        afterResize: function () {
+            var pluginContainer = $(this);
+            $.fn.fullpage.reBuild();
+        },
+        //events
+        onLeave: function(origin, destination, direction, index){},
+        afterLoad: function(origin, destination, direction){
+            if(origin != 'section01'){
+                $('.btn_toTop').addClass('active')
+            }else{
+                $('.btn_toTop').removeClass('active')
+            }
+        },
+        afterRender: function(){},
+        afterReBuild: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(section, origin, destination, direction){},
+        onSlideLeave: function(section, origin, destination, direction, index){}
+    });
 
-	
-	// 6. Prepare content for animation
-	$('.section .content .anim.anim-wrapped').wrap("<span class='anim-wrapper'></span>");
+}
 
-	// 7. Init fullPage.js plugin
-	var pageSectionDivs = $('.page-fullpage .section');
-	var headerContainer = $('.hh-header');
-	var slideElem = $('.slide');
-	var arrowElem = $('.p-footer .arrow-d');
-	var pageElem = $('.section');
-	var pageSections = [];
-	var pageAnchors = [];
-	var mainPage = $('#mainpage');
-	var scrollOverflow = true;
-	var css3 = true;
-	// disable scroll overflow on small device
-	if (contextWindow.width() < 601) {
-		scrollOverflow = false;
-		css3 = false;
-	}
-	if (contextWindow.height() < 480) {
-		scrollOverflow = false;
-		css3 = false;
-	}
-	// Get sections name
-	for (var i = 0; i < pageSectionDivs.length; i++) {
-		pageSections.push(pageSectionDivs[i]);
-	}
-	window.asyncEach(pageSections, function (pageSection, cb) {
-		var anchor = pageSection.getAttribute('data-section');
-		pageAnchors.push(anchor + "");
-		cb();
-	}, function (err) {
-		// Init plugin
-		if (mainPage.width()) {
-			// config fullpage.js
-			mainPage.fullpage({
-				menu: '#qmenu',
-				anchors: pageAnchors,
-				verticalCentered: false,
-				css3: css3,
-				navigation: true,
-				responsiveWidth: 601,
-				responsiveHeight: 480,
-				// scrollOverflow: scrollOverflow,
-				// scrollOverflowOptions: {
-				// 	click: true,
-				// 	submit: true,
-				// },
-				normalScrollElements: '.section .scrollable',
-				afterRender: function () {
-					
-					// Fix for internet explorer : adjust content height
-					// Detect IE 6-11
-					var isIE = /*@cc_on!@*/false || !!document.documentMode;
-					if (isIE) {
-						var contentColumns = $('.section .content .c-columns');
-						contentColumns.height(contextWindow.height())
-						for (var i = 0; i < contentColumns.length; i++) {
-							if (contentColumns[i].height <= contextWindow.height()) {
-								contentColumns[i].style.height = "100vh";
-							}
-						}
-					}
-				},
-				afterResize: function () {
-					var pluginContainer = $(this);
-					$.fn.fullpage.reBuild();
-				},
-				onLeave: function (index, nextIndex, direction) {
-					// Behavior when a full page is leaved
-					arrowElem.addClass('gone');
-					pageElem.addClass('transition');
-					slideElem.removeClass('transition');
-					pageElem.removeClass('transition');
-				},
-				afterLoad: function (anchorLink, index) {
-					// Behavior after a full page is loaded
-					// hide or show clock
-					if ($('.section.active').hasClass('hide-clock')) {
-						headerContainer.addClass('gone');
-					} else {
-						headerContainer.removeClass('gone');
-					}
-				}
-			});
+$('.navigationToggle').click(function(){
+    $(this).closest('.site-header').toggleClass('active');
+    $('#fp-nav').fadeOut();
+    $('.navigationToggle').fadeOut();
+})
 
-		}
-	});
-	// Scroll to fullPage.js next/previous section
-	$('.scrolldown a, .scroll.down').on('click', function () {
-		try {
-			// fullpage scroll
-			$.fn.fullpage.moveSectionDown();
-		} catch (error) {
-			// normal scroll
-			$root.animate({
-				scrollTop: window.innerHeight
-			}, 400, function () {
-			});
-		}
+$('.closeMenu').click(function(){
+    $(this).closest('.site-header').removeClass('active');
+    $('#fp-nav').show();
+    $('.navigationToggle').show();
+})
 
-	});
-
-	// 8. Hide some ui on scroll
-	var scrollHeight = $(document).height() - contextWindow.height();
-	contextWindow.on('scroll', function () {
-		var scrollpos = $(this).scrollTop();
-		var siteHeaderFooter = $('.page-footer, .page-header');
-
-		// if (scrollpos > 10 && scrollpos < scrollHeight - 100) {
-		if (scrollpos > 100) {
-			siteHeaderFooter.addClass("scrolled");
-		}
-		else {
-			siteHeaderFooter.removeClass("scrolled");
-		}
-	});
-
-
-	// 9. Page Loader : hide loader when all are loaded
-	contextWindow.on('load', function () {
-		$('#page-loader').addClass('p-hidden');
-		$('.section').addClass('anim');
-	});
-
-
-});
-
+$('.selectUltilites figure').click(function(){
+    $('#exampleModal').modal('show');
+    var value = $(this).html();
+    $('.modal .modal-body').html(value);
+})
